0d5e9ae38ffa0ad4b08a11c09826075934882aa8
Cindy <lecindy95@gmail.com>
2020-06-23 10:36:03 -0700

    added client folder

---
+++ client/build/precache-manifest.ca8e454d6c4f8f7175008fbff1458a6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17432398866a3998ef8ff1710d0752cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5192d6e9acbf60ac54eb",
+    "url": "/static/css/main.bccd0344.chunk.css"
+  },
+  {
+    "revision": "743cb3f389247e8c735d",
+    "url": "/static/js/2.b4006025.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b4006025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5192d6e9acbf60ac54eb",
+    "url": "/static/js/main.0c409eef.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
